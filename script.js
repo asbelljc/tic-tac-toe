19a6e8b0d823a2349e45d8677acd7bd0684c40a8
asbelljc <asbelljc@gmail.com>
2021-07-31 22:53:12 -0400

    Did some testing and 'check' methods do not work. Noted in comments. Figure out logic next time...

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 const gameboard = (() => {
-  let _board = [ [], [], [] ];
+  // placeholders keep rows of 'undefined' from 'winning' and simplify code
+  let _board = [ [1,2,3], [4,5,6], [7,8,9] ];
   let _token = "#"; // figure out how to make this X for player / O for CPU
   const _declareResult = () => {
     // something...
     console.log("test");
   };
-  const _checkColumns = () => {
-    _board.every(row => {
+  const _checkColumns = () => { // this doesn't work
+    return _board.every(row => {
       return row[0] === "X" || row[1] === "X" || row[2] === "x" ||
              row[0] === "O" || row[1] === "O" || row[2] === "O"
     });
   }
-  const _checkRows = () => {
-    _board.some(row => row[0] === row[1] && row[1] === row[2]);
+  const _checkRows = () => { // this doesn't work
+    return _board.some(row => row[0] === row[1] && row[1] === row[2]);
   }
-  const _checkDiagonals = () => {
-    return _board[0][0] === _board[1][1] && _board[1][1] === _board[2][2] ||
-           _board[0][2] === _board[1][1] && _board[1][1] === _board[2][0]
+  const _checkDiagonals = () => { // this seems to work now but looks... clunky
+    return ((_board[0][0] === _board[1][1]) && (_board[1][1] === _board[2][2])) ||
+           ((_board[0][2] === _board[1][1]) && (_board[1][1] === _board[2][0]))
   }
   const _checkForEnd = () => {
     if (_checkColumns() || _checkRows() || _checkDiagonals) {
@@ -27,4 +28,6 @@ const gameboard = (() => {
     _board[x][y] = _token;
     _checkForEnd();
   };
+
+  return { _board, _checkForEnd, _checkColumns, _checkRows, _checkDiagonals, play };
 })();
