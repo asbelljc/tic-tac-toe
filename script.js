44386d45c61d43808c5507c3c96611a235083a1b
asbelljc <asbelljc@gmail.com>
2021-08-17 19:29:49 -0400

    Refactored controller to dispatch custom events to game module upon click. Updated corresponding functions in game and display modules. Began updating checkForEnd function in game module.

--- script.js
+++ script.js
@@ -19,31 +19,31 @@ const display = (() => {
   // menus
   const startMenu = document.getElementById("start-menu");
   const onePlayerMenu = document.getElementById("one-player-menu");
-    const slider = document.getElementById("slider");
+  const slider = document.getElementById("slider");
   const twoPlayerMenu = document.getElementById("two-player-menu");
   // scoreboard
   const scoreboard = document.getElementById("scoreboard");
-    const playerOneName = document.querySelector("#player-one .name");
-      const playerOneScore = document.querySelector("#player-one .score");
-    const playerTwoName = document.querySelector("#player-two .name");
-      const playerTwoScore = document.querySelector("#player-two .score");
-    const messageBox = document.getElementById("message-box");
-      const message = document.getElementById("message");
+  const playerOneName = document.querySelector("#player-one .name");
+  const playerOneScore = document.querySelector("#player-one .score");
+  const playerTwoName = document.querySelector("#player-two .name");
+  const playerTwoScore = document.querySelector("#player-two .score");
+  const messageBox = document.getElementById("message-box");
+  const message = document.getElementById("message");
   // gameboard
   const gameboard = document.getElementById("gameboard");
-    const squares = document.getElementsByClassName("square");
-    const newGameBtn = document.getElementById("new-game");
+  const squares = document.getElementsByClassName("square");
+  const newGameBtn = document.getElementById("new-game");
 
   const changeScreen = e => {
-      // start menu options
-    if (e.target.innerText === "1 player") {
+    // start menu options
+    if (e.detail.mode === "1 player") {
       startMenu.classList.add("hidden");
       onePlayerMenu.classList.remove("hidden");
-    } else if (e.target.innerText === "2 player") {
+    } else if (e.detail.mode === "2 player") {
       startMenu.classList.add("hidden");
       twoPlayerMenu.classList.remove("hidden");
       // 1- and 2-player options both do the same thing
-    } else if (e.target.className === "start") {
+    } else if (e.detail.button === "start") {
       onePlayerMenu.classList.add("hidden");
       playerOneName.innerText === game.playerOne.getName();
       playerOneScore.innerText === 0;
@@ -59,11 +59,11 @@ const display = (() => {
       gameboard.classList.add("hidden");
       newGameBtn.classList.add("hidden");
     }
-  } 
+  }
 
   const renderCurrentBoard = () => {
     let currentBoard = game.getBoard();
-    for (let i=0; i<squares.length; i++) {
+    for (let i = 0; i < squares.length; i++) {
       if (typeof currentBoard[i] === "number") {
         squares[i].innerText = "";
       } else {
@@ -73,7 +73,7 @@ const display = (() => {
   }
 
   const showDifficulty = e => {
-    slider.className = `slider ${e.target.innerText}`;
+    slider.className = `slider ${e.detail.difficulty}`;
   }
 
   return {
@@ -88,49 +88,62 @@ const display = (() => {
 game = (() => {
   let mode; // single or multi-player
   let difficulty = "medium"; // establish default diffulty
+  let turn = 1;
   let playerOne;
   let playerTwo;
 
   // integer placeholders keep rows of 'undefined' from 'winning' and simplify code
-  let board = [0,1,2,3,4,5,6,7,8];
+  let board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
+  const winLines = [
+    [0, 1, 2], // rows
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6], // columns
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8], // diagonals
+    [2, 4, 6]
+  ];
 
   const getBoard = () => board;
 
   const fillSquare = e => {
-    board[parseInt(e.target.id)] = mode === "single" ? "X" : "O";
-    display.renderCurrentBoard();
+    // check if square is empty before 
+    if (typeof board[parseInt(e.detail.square)] === "number") {
+      // odd turns should always mark X (because X starts in tic-tac-toe)
+      board[parseInt(e.detail.square)] = turn % 2 === 1 ? "X" : "O";
+      display.renderCurrentBoard();
+      checkForEnd();
+      turn++;
+    }
   };
 
   const checkForEnd = () => {
-    if (
-      board[0] === board[1] && board[1] === board[2] || // rows
-      board[3] === board[4] && board[4] === board[5] || // .
-      board[6] === board[7] && board[7] === board[8] || // .
-      board[0] === board[3] && board[3] === board[6] || // columns
-      board[1] === board[4] && board[4] === board[7] || // .
-      board[2] === board[5] && board[5] === board[8] || // .
-      board[0] === board[4] && board[4] === board[8] || // diagonals
-      board[2] === board[4] && board[4] === board[6]    // .
-    ) { console.log("[working code]") }
+    const isWin = winLines.some(winLine => {
+      
+    });
   };
 
-  const setMode = (e) => {
-    mode = e.target.innerText === "1 player" ? "single" : "multi";
+  const setMode = e => {
+    mode = e.detail.mode === "1 player" ? "single" : "multi";
     display.changeScreen(e);
   };
 
-  const setDifficulty = (e) => {
-    difficulty = e.target.innerText;
+  const setDifficulty = e => {
+    difficulty = e.detail.difficulty;
     display.showDifficulty(e);
   };
 
+  const start = e => {
+    turn = 1;
+    display.changeScreen(e);
+  };
+
+  document.addEventListener("setDifficulty", setDifficulty);
+
   return {
-    setMode,
-    setDifficulty,
-    start,
-    fillSquare,
-    getBoard,
-    reset
+    getBoard
   };
 })();
 
@@ -145,15 +158,41 @@ const controller = (() => {
   const squares = Array.from(document.getElementsByClassName("square"));
   const newGameBtn = document.getElementById("new-game");
 
-  gameModeButtons.forEach(button => button.addEventListener("click", game.setMode));
-  difficultyBtns.forEach(button => button.addEventListener("click", game.setDifficulty));
-  startBtns.forEach(button => button.addEventListener("click", game.start));
-  squares.forEach(square => square.addEventListener("click", game.fillSquare));
-  newGameBtn.addEventListener("click", game.reset);
+  // convert clicks to custom events for the game module to catch
+
+  gameModeButtons.forEach(button => button.addEventListener("click", e => {
+    document.dispatchEvent(
+      new CustomEvent("setMode", { detail: { mode: e.target.innerText } })
+    );
+  }));
+
+  difficultyBtns.forEach(button => button.addEventListener("click", e => {
+    document.dispatchEvent(
+      new CustomEvent("setDifficulty", { detail: { difficulty: e.target.innerText } })
+    );
+  }));
+
+  startBtns.forEach(button => button.addEventListener("click", e => {
+    document.dispatchEvent(
+      new CustomEvent("start", { detail: { button: e.target.className } })
+    );
+  }));
+
+  squares.forEach(square => square.addEventListener("click", e => {
+    document.dispatchEvent(
+      new CustomEvent("fillSquare", { detail: { square: e.target.id } })
+    )
+  }));
+
+  newGameBtn.addEventListener("click", e => {
+    document.dispatchEvent(
+      new CustomEvent("reset", { detail: { button: e.target.id } })
+    );
+  });
 
   const getNames = () => nameInputs.map(input => input.innerText);
   const resetNameInputs = () => nameInputs.forEach(input => input.innerText === "");
-  
+
   return {
     getNames,
     resetNameInputs
