05121ac5c40af1d00925224f47871762d4662893
asbelljc <asbelljc@gmail.com>
2021-08-17 00:09:50 -0400

    Most likely finished controller module. Some of the triggered functions, like game.start and game.reset, are not written in the game module yet. Next time...

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 let game;
 
-const Player = (name, difficulty) => {
+const Player = (name) => {
   let score = 0;
   const getScore = () => score;
   const getName = () => name;
-  const getDifficulty = () => difficulty;
   const win = () => score++;
 
   return {
     getScore,
     getName,
-    getDifficulty,
     win
-  }
+  };
 };
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -34,6 +32,34 @@ const display = (() => {
   // gameboard
   const gameboard = document.getElementById("gameboard");
     const squares = document.getElementsByClassName("square");
+    const newGameBtn = document.getElementById("new-game");
+
+  const changeScreen = e => {
+      // start menu options
+    if (e.target.innerText === "1 player") {
+      startMenu.classList.add("hidden");
+      onePlayerMenu.classList.remove("hidden");
+    } else if (e.target.innerText === "2 player") {
+      startMenu.classList.add("hidden");
+      twoPlayerMenu.classList.remove("hidden");
+      // 1- and 2-player options both do the same thing
+    } else if (e.target.className === "start") {
+      onePlayerMenu.classList.add("hidden");
+      playerOneName.innerText === game.playerOne.getName();
+      playerOneScore.innerText === 0;
+      playerTwoName.innerText === game.playerTwo.getName();
+      playerTwoScore.innerText === 0;
+      scoreboard.classList.remove("empty");
+      gameboard.classList.remove("hidden");
+      newGameBtn.classList.remove("hidden");
+      // new-game button circles back to start menu
+    } else if (e.target.id === "new-game") {
+      onePlayerMenu.classList.remove("hidden");
+      scoreboard.classList.add("empty");
+      gameboard.classList.add("hidden");
+      newGameBtn.classList.add("hidden");
+    }
+  } 
 
   const renderCurrentBoard = () => {
     let currentBoard = game.getBoard();
@@ -44,26 +70,28 @@ const display = (() => {
         squares[i].innerText = currentBoard[i];
       }
     }
-  };
+  }
 
-  function moveSlider(e) {
+  const showDifficulty = e => {
     slider.className = `slider ${e.target.innerText}`;
   }
 
   return {
-    moveSlider,
-    renderCurrentBoard
-  }
+    changeScreen,
+    renderCurrentBoard,
+    showDifficulty
+  };
 })();
 
 ///////////////////////////////////////////////////////////////////////////////
 
 game = (() => {
   let mode; // single or multi-player
+  let difficulty = "medium"; // establish default diffulty
   let playerOne;
   let playerTwo;
 
-  // placeholders keep rows of 'undefined' from 'winning' and simplify code
+  // integer placeholders keep rows of 'undefined' from 'winning' and simplify code
   let board = [0,1,2,3,4,5,6,7,8];
 
   const getBoard = () => board;
@@ -86,9 +114,23 @@ game = (() => {
     ) { console.log("[working code]") }
   };
 
+  const setMode = (e) => {
+    mode = e.target.innerText === "1 player" ? "single" : "multi";
+    display.changeScreen(e);
+  };
+
+  const setDifficulty = (e) => {
+    difficulty = e.target.innerText;
+    display.showDifficulty(e);
+  };
+
   return {
+    setMode,
+    setDifficulty,
+    start,
+    fillSquare,
     getBoard,
-    fillSquare
+    reset
   };
 })();
 
@@ -96,21 +138,24 @@ game = (() => {
 
 const controller = (() => {
   // buttons and inputs
-  const newGameBtn = document.getElementById("new-game");
+  const gameModeButtons = Array.from(document.getElementsByClassName("player-count"));
   const difficultyBtns = Array.from(document.getElementsByClassName("level"));
   const startBtns = Array.from(document.getElementsByClassName("start"));
   const nameInputs = Array.from(document.getElementsByClassName("name-input"));
-    const playerInput = nameInputs[0];
-    const playerOneInput = nameInputs[1];
-    const playerTwoInput = nameInputs[2];
   const squares = Array.from(document.getElementsByClassName("square"));
+  const newGameBtn = document.getElementById("new-game");
 
-  difficultyBtns.forEach(button => {
-    button.addEventListener("click", e => {
-      display.moveSlider(e);
-      // setDifficulty();
-    });
-  });
-
+  gameModeButtons.forEach(button => button.addEventListener("click", game.setMode));
+  difficultyBtns.forEach(button => button.addEventListener("click", game.setDifficulty));
+  startBtns.forEach(button => button.addEventListener("click", game.start));
   squares.forEach(square => square.addEventListener("click", game.fillSquare));
+  newGameBtn.addEventListener("click", game.reset);
+
+  const getNames = () => nameInputs.map(input => input.innerText);
+  const resetNameInputs = () => nameInputs.forEach(input => input.innerText === "");
+  
+  return {
+    getNames,
+    resetNameInputs
+  };
 })();
